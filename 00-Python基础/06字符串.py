32315677b05207fd253090e95cb84741ae889ffd
luguanxing <651571925@qq.com>
2017-02-11 22:55:57 +0800

    字符串

---
+++ 00-Python基础/06字符串.py
@@ -0,0 +1,18 @@
+str = "abcdefgh"
+
+print("str = 'abcdefgh'")
+print(str)
+print("\n")
+
+print(str.capitalize())
+print(str.center(5))
+print(str.endswith("fgh"))
+print(str.find("sdfas"))
+print("X".join(str))
+print(str.replace("b","K"))
+print(str.split())
+print()
+print()
+print()
+
+
