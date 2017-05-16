d70eae10c8b22e2855e89691919370823f698ca1
luguanxing <651571925@qq.com>
2017-05-16 09:39:50 +0800

    字符串

--- 00-Python基础/06字符串.py
+++ 00-Python基础/06字符串.py
@@ -1,18 +1,30 @@
 str = "abcdefgh"
-
 print("str = 'abcdefgh'")
-print(str)
-print("\n")
+print()
 
+print("str.capitalize()")
 print(str.capitalize())
-print(str.center(5))
+print()
+
+print("str.center(20), 'a'")
+print(str.center(20, '-'))
+print()
+
+print("str.endswith('fgh')")
 print(str.endswith("fgh"))
+print()
+
+print("str.find(\"sdfas\")")
 print(str.find("sdfas"))
+print()
+
+print("'X'.join(str)")
 print("X".join(str))
+print()
+
+print("str.replace('b','K')")
 print(str.replace("b","K"))
+print()
+
+print("str.split()")
 print(str.split())
-print()
-print()
-print()
-
-
